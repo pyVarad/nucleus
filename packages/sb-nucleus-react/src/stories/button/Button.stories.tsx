3318ab165df042d7af6add6f2f4c14059ba14a35
Varad <py.varad@gmail.com>
2023-12-05 18:55:27 +0530

    Updated storybook binding for button component

--- packages/sb-nucleus-react/src/stories/button/Button.stories.tsx
+++ packages/sb-nucleus-react/src/stories/button/Button.stories.tsx
@@ -1,18 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { buttonArgsType } from './button.args';
-import { Button } from './Button';
+import { buttonArgsType } from './Button.args';
+import { NucleusButton } from 'nucleus-react';
+import { ReactNode } from 'react';
 
 const meta = {
   title: "component/Button",
   tags: ['autodocs'],
-  component: Button,
+  component: NucleusButton,
   argTypes: buttonArgsType,
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof NucleusButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
+  render: (args) => {
+    NucleusButton.displayName = 'NucleusButton';
+    const buttonType = args.buttontype || 'primary';
+    const label: ReactNode = ('label' in args ? args.label : 'Primary') as ReactNode;
+    return <NucleusButton buttontype={buttonType}>{label}</NucleusButton>
+
+  }
 };
 
 export const callToAction: Story = {
